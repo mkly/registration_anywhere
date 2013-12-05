44e6aa9c22efee57731c429e0b587f4bab174ca0
Mike Lay <mike@mkly.io>
2013-12-05 15:08:12 -0800

    Code cleanup

--- registration_anywhere/blocks/registration_anywhere/templates/table.php
+++ registration_anywhere/blocks/registration_anywhere/templates/table.php
@@ -1,114 +1,110 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
-<?php if($show_registration_disabled): ?>
+<?php if ($show_registration_disabled) { ?>
 	<div class="registration-anywhere">
 		<p>
-			<?php echo $registration_disabled_text ?>
+			<?= $registration_disabled_text ?>
 		</p>
 	</div>
-<?php elseif($show_form): ?>
+<?php } else if ($show_form) { ?>
 	<div class="registration-anywhere">
 		<table>
-			<form id="registration-anywhere" name="registration-anywhere" method="post" action="<?php echo $this->url('/register', 'do_register') ?>">
+			<form id="registration-anywhere" name="registration-anywhere" method="post" action="<?= $this->url('/register', 'do_register') ?>">
 				<thead>
 					<tr>
 						<th colspan="2">
-							<h2><?php echo t($form_title); ?></h2>
+							<h2><?= t($form_title) ?></h2>
 						</th>
 					</tr>
 				</thead>
 				<tbody>
-					<?php if($show_details_header): ?>
+					<?php if ($show_details_header) { ?>
 						<tr>
 							<td colspan="2">
-								<h3><?php echo $details_header ?></h3>
+								<h3><?= $details_header ?></h3>
 							</td>
 						</tr>
-					<?php endif; ?>
-					<?php if($display_username_field): ?>
+					<?php } ?>
+					<?php if ($display_username_field) { ?>
 						<tr>
 							<td>
-								<?php echo $form->label('uName', t('Username') . '<pan class="ccm-required">*</span>' ) ?>
+								<?= $form->label('uName', t('Username') . '<pan class="ccm-required">*</span>' ) ?>
 							</td>
 							<td>
-								<?php echo $form->text('uName') ?>
+								<?= $form->text('uName') ?>
 							</td>
 						</tr>
-					<?php endif; ?>
+					<?php } ?>
 					<tr>
 						<td>
-							<?php echo $form->label('uEmail', t('Email Address') . '<span class="ccm-required">*</span>' ) ?>
+							<?= $form->label('uEmail', t('Email Address') . '<span class="ccm-required">*</span>' ) ?>
 						</td>
 						<td>
-							<?php echo $form->text('uEmail') ?>
+							<?= $form->text('uEmail') ?>
 						</td>
 					</tr>
 					<tr>
 						<td>
-							<?php echo $form->label('uPassword', t('Password') . '<span class="ccm-required">*</span>' ) ?>
+							<?= $form->label('uPassword', t('Password') . '<span class="ccm-required">*</span>' ) ?>
 						</td>
 						<td>
-							<?php echo $form->password('uPassword') ?>
+							<?= $form->password('uPassword') ?>
 						</td>
 					</tr>
 					<tr>
 						<td>
-							<?php echo $form->label('uPasswordConfirm', t('Confirm Password') . '<span class="ccm-required">*</span>') ?>
+							<?= $form->label('uPasswordConfirm', t('Confirm Password') . '<span class="ccm-required">*</span>') ?>
 						</td>
 						<td>
-							<?php echo $form->password('uPasswordConfirm') ?>
+							<?= $form->password('uPasswordConfirm') ?>
 						</td>
 					</tr>
 	
-					<?php if($show_options_header): ?>
+					<?php if ($show_options_header) { ?>
 						<tr>
 							<td colspan="2">
-								<h3><?php echo $options_header ?></h3>
+								<h3><?= $options_header ?></h3>
 							</td>
 						</tr>
-					<?php endif; ?>
-					<?php foreach($lighter_attributes as $attribute): ?>
+					<?php } ?>
+					<?php foreach ($lighter_attributes as $attribute) { ?>
 						<tr>
 							<td>
-								<?php if($attribute['required']): ?>
-									<?php echo $form->label($attribute['id'], $attribute['name'] . '<span class="ccm-required">*</span>') ?>
-								<?php else: ?>
-									<?php echo $form->label($attribute['id'], $attribute['name']) ?>
-								<?php endif; ?>
+								<?php if ($attribute['required']) { ?>
+									<?= $form->label($attribute['id'], $attribute['name'] . '<span class="ccm-required">*</span>') ?>
+								<?php } else { ?>
+									<?= $form->label($attribute['id'], $attribute['name']) ?>
+								<?php } ?>
 							</td>
 							<td>
-								<?php echo $attribute['form'] ?>
+								<?= $attribute['form'] ?>
 							</td>
 						</tr>
-					<?php endforeach; ?>
+					<?php } ?>
 					<tr>
 						<td>
-							<?php if($pre_55): ?>
-								<?php echo $form->label('captcha', t('Please type the letters and numbers shown in the image')); ?>
-							<?php else: ?>
 								<?php echo $captcha->label() ?>
-							<?php endif; ?>
 						</td>
 						<td>
-							<?php $captcha->showInput(); ?>
+							<?php $captcha->showInput() ?>
 						</td>
 					</tr>
 					<tr>
 						<td></td>
 						<td>
-							<?php $captcha->display(); ?>
+							<?php $captcha->display() ?>
 						</td>
 					</tr>
 					<tr>
-						<td colspan="2"><?php echo $form->submit('register', t('Register') . ' &gt;'); ?></td>
+						<td colspan="2"><?= $form->submit('register', t('Register') . ' &gt;') ?></td>
 					</tr>
 				</tbody>
 			</form>
 		</table>
 	</div>
-<?php elseif($show_logged_in): ?>
+<?php } else if ($show_logged_in) { ?>
 	<div class="registration-anywhere">
 		<p>
-			<?php echo $logged_in_as . ' ' . $display_name ?>
+			<?= $logged_in_as . ' ' . $display_name ?>
 		</p>
 	</div>
-<?php endif; ?>
+<?php } ?>
