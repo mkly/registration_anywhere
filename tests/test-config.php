d0e247885e87cbd5107a034d72b65d3d6629c8d1
Mike Lay <mike@mkly.io>
2013-12-12 22:53:46 -0800

    Fix database in tests

--- tests/test-config.php
+++ tests/test-config.php
@@ -1,9 +1,8 @@
 <?php
 $config = array(
 	'db-server' => 'localhost',
-	'db-username' => 'root',
+	'db-username' => 'travis',
 	'db-database' => 'test',
-	'db-password' => 'root',
 	'admin-password' => 'password',
 	'target' => dirname(__FILE__) . '/core/concrete5/web',
 	'starting-point' => 'registration_anywhere_test'
