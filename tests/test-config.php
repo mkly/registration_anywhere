f0e3eb8c21de1e2b5e9f047b868755cbf090b357
Mike Lay <mike@mkly.io>
2013-12-12 22:52:03 -0800

    Fix sample content paths

--- tests/test-config.php
+++ tests/test-config.php
@@ -1,9 +1,10 @@
 <?php
 $config = array(
 	'db-server' => 'localhost',
-	'db-username' => 'travis',
+	'db-username' => 'root',
 	'db-database' => 'test',
+	'db-password' => 'root',
 	'admin-password' => 'password',
 	'target' => dirname(__FILE__) . '/core/concrete5/web',
-	'starting-point' => 'registration_anywhere'
+	'starting-point' => 'registration_anywhere_test'
 );
